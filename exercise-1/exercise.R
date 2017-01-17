227d42effcc111b269524f69b2bd2ab6d09e62f5
Coco xu <tufanx@uw.edu>
2017-01-17 15:02:44 -0800

    Go seahwaks

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,11 +2,11 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-seahwaks <- c(50:70)
+seahwaks <- rnorm(5)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-points <- c(20:40)
+points <- rnorm(5)
 
 # Combine your two vectors into a dataframe
 versus <- data.frame(points,seahwaks)
@@ -18,13 +18,14 @@ dif <- abs(points-seahwaks)
 versus <- data.frame(seahwaks,points,dif)
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+won <- seahwaks>points
+versus <- data.frame(seahwaks,points,dif,won)
 
 # Create a vector of the opponent names corresponding to the games played
-opponents <- letters[1:20]
+opponents <- letters[1:21]
 
 # Assign your dataframe rownames of their opponents
 rownames(versus) <- opponents
 
 # View your data frame to see how it has changed!
-versus
+View(versus)
