a61113126ce354d64018518d774414eb00243883
Coco xu <tufanx@uw.edu>
2017-01-17 18:08:23 -0800

    huskygames

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,39 +1,45 @@
 # Exercise 6: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-
+#husky.games.2015 <- read.csv("huskies_2015.csv")
+husky.games.2015 <- read.csv("data/huskies_2015.csv",stringsAsFactors = F)
 
 
 # Create a vector of the teams that the Huskies played against during that season
-
-
+teams <- husky.games.2015$opponent
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-
+husky.scores <- husky.games.2015$uw_score
 
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
-
+rushing.yards <- husky.games.2015$rushing_yards
+passing.yards <- husky.games.2015$passing_yards
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
-
+combined.yards <- rushing.yards + passing.yards
 
 
 # Add the `combined.yards` vector to the dataframe with the column name being `combined`
-
-
+husky.games.2015$combined <- combined.yards
+# xxx$newcolumnname <- new dataset
 
 # Write the dataframe to a new `.csv` file called `huskies_2015_improved.csv` into the `data directory`
-
+write.csv(husky.games.2015, "huskies_2015_improved.csv")
 
 
 # What is the score of the game where the Huskies had the most combined yards?
-
+most.yards <- max(husky.games.2015$combined)
 
 
 # Define a function `MostYardsScore` that takes in an argument `games` (a data frame) and
-# returns a descriptive sentence about the game that was played that had the most yards scored in it.
+# returns a descriptive sentence about the game that was played that had the most yards scored in it
+MostYardsScore <- function(games,most.yards) {
+  View(games)
+  highest.score <- games[games$combined==most.yards]
+  paste("On",games$date,", UW Husky played against", games$opponent,"passing yard is",games$passing_yards,"and rushing yard is",
+               games$rushing_yards,", and the most yards score in it were ", max(games$combined))
+}
 
 
 
